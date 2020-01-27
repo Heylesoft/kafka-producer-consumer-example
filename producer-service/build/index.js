88643a3f2c2b55baec3c58c0851ac72002f89d55
Jefferson Ortiz Quiroga <jeffliveco16@gmail.com>
2020-01-27 05:01:52 -0500

    GSS-22 Add kafka dependency

--- producer-service/build/index.js
+++ producer-service/build/index.js
@@ -1,7 +1,36 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const kafka_node_1 = require("kafka-node");
 class Main {
     constructor() {
         console.log('Hello API');
+        this.client = new kafka_node_1.KafkaClient({ kafkaHost: 'localhost:9092' });
+        this.producer = new kafka_node_1.Producer(this.client);
+    }
+    createTopics() {
+        const topicsToCreate = [
+            {
+                topic: 'topic1',
+                partitions: 1,
+                replicationFactor: 2
+            },
+            {
+                topic: 'topic2',
+                partitions: 2,
+                replicationFactor: 2
+            },
+        ];
+        this.client.createTopics(topicsToCreate, (err, result) => {
+            if (err)
+                console.log('error', err);
+            if (result) {
+                result.forEach((createResponse) => {
+                    console.log('createResponse', createResponse);
+                });
+            }
+        });
     }
 }
 const main = new Main();
+main.createTopics();
 //# sourceMappingURL=index.js.map
--- producer-service/src/index.ts
+++ producer-service/src/index.ts
@@ -1,8 +1,39 @@
+import Kafka, { Producer, KafkaClient, CreateTopicRequest, CreateTopicResponse } from 'kafka-node';
 
 class Main {
+  private producer:Producer;
+  private client:KafkaClient;
+
   constructor(){
     console.log('Hello API');
+    this.client = new KafkaClient({kafkaHost: 'localhost:9092'});
+    this.producer = new Producer(this.client);
+  }
+
+  createTopics ():void {
+    const topicsToCreate:CreateTopicRequest[] = [
+      {
+        topic: 'topic1',
+        partitions: 1,
+        replicationFactor: 2
+      },
+      {
+        topic: 'topic2',
+        partitions: 2,
+        replicationFactor: 2
+      },
+    ];
+
+    this.client.createTopics(topicsToCreate, (err:any, result:CreateTopicResponse[]):void => {
+      if (err) console.log('error', err);
+      if(result){
+        result.forEach((createResponse:CreateTopicResponse) => {
+          console.log('createResponse', createResponse);
+        });
+      }
+    });
   }
 }
 
 const main:Main = new Main();
+main.createTopics();
